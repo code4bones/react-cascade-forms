67862c71a4e78f64989daa73091f8c383d2ba8ba
Administrator <admin@undoo.ru>
2023-01-02 15:35:02 +0300

    clean

--- rollup.config.js
+++ rollup.config.js
@@ -2,10 +2,6 @@ import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "rollup-plugin-typescript2";
-import less from "rollup-plugin-less";
-// import postcss from "rollup-plugin-postcss";
-import copy from "rollup-plugin-copy";
-// import image from "@rollup/plugin-image";
 const packageJson = require("./package.json");
 
 export default {
@@ -30,20 +26,5 @@ export default {
 		commonjs(),
 		// image(),
 		typescript({ useTsconfigDeclarationDir: true }),
-		less(),
-		copy({
-			targets: [
-				{
-					src: "src/variables.less",
-					dest: "build",
-					rename: "variables.less"
-				},
-				{
-					src: "rollup.build.css",
-					dest: "build",
-					rename: "styles.css"
-				},
-			]
-		})
 	]
 };

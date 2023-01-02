6fb92bc5595bd5f75f07437768c08e18eed0e330
Administrator <admin@undoo.ru>
2023-01-02 15:36:51 +0300

    1.0.4

--- build/CascadeForms/CascadeForms.d.ts
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { FormActions, ControlItemProps } from "./CascadeForms.types";
-export * from "./CascadeForms.types";
-declare const CascadeForms: React.ForwardRefExoticComponent<{
-    form: import("./CascadeForms.types").FormItems;
-} & Omit<ControlItemProps, "item"> & React.RefAttributes<FormActions>>;
-export default CascadeForms;
--- build/index.d.ts
+++
@@ -1,3 +0,0 @@
-import CascadeForms, { OnChangeFn, ControlRenderProps, FormActions, FormItem, FormItems, FormState } from "./CascadeForms/CascadeForms";
-export { OnChangeFn, ControlRenderProps, FormItem, FormState, FormActions, FormItems, };
-export default CascadeForms;
